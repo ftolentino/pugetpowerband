9b6f9f75b4c4e5964c8ad7fdb2dd703356091333
ftolentino <filmer.tolentino@gmail.com>
2021-12-16 16:07:24 -0800

    set up mobile nav menu and button, need figure out how to make the mobile nav show when clicked

--- src/components/Navbar_t.js
+++ src/components/Navbar_t.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'gatsby';
 import { ImPower } from '@react-icons/all-files/im/ImPower';
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import '../styles/global.css';
 
 
 const Navbar_t = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () =>setClick(false);
   return (
     <div>
       <nav className='bg-red-300'>
-        <div className="max-w-screen-sm xl:max-w-6xl mx-auto">
+        <div className="max-w-screen-sm xl:max-w-full mx-auto px-4">
           <div className="flex justify-between">
             <div className="flex space-x-4">
               {/* logo */}
               <div>
-                <Link to='/' className='flex items-center py-2 px-2 text-yellow-100 hover:text-indigo-400 transition duration-300'>
+                <Link to='/' className='flex items-center py-2 px-2 text-yellow-100 hover:text-indigo-400 transition duration-300' onClick={closeMobileMenu}>
                   <ImPower className='h-10 w-10 mr-1'/>
                   <span className='font-bold text-6xl'> 
                     PP
@@ -23,15 +28,27 @@ const Navbar_t = () => {
               </div>   
             </div>
             {/* primary nav */}
-            <div className='flex items-center space-x-3 font-semibold text-yellow-900 text-3xl'>
+            <div className='hidden md:flex items-center space-x-3 font-semibold text-yellow-900 text-3xl'>
               <Link to="/music" className='flex items-center py-2 px-2 hover:text-yellow-100'>Music</Link>
               <Link to="/media" className='flex items-center py-2 px-2 hover:text-yellow-100'>Media</Link>
               <Link to="/about" className='flex items-center py-2 px-2 hover:text-yellow-100'>About</Link>
               <Link to="/contact" className='flex items-center py-2 px-2 hover:text-yellow-100'>Contact</Link>
             </div>
+            {/* mobile button */}
+            <div className='md:hidden flex items-center'>
+              <button className='mobile-menu-button' onClick={handleClick}>
+                {click ? <FaTimes className='w-8 h-8'/> : <FaBars className='w-8 h-8'/>}
+              </button>
+            </div>
           </div>
         </div>
-            {/* mobile menu */}  
+        {/* mobile menu */}  
+        <div className='mobile-menu hidden md:hidden text-yellow-900'>
+          <Link to="/music" className='block py-2 px-2 text-sm hover:bg-red-400 hover:text-yellow-100' onClick={closeMobileMenu}>Music</Link>
+          <Link to="/media" className='block py-2 px-2 text-sm hover:bg-red-400 hover:text-yellow-100' onClick={closeMobileMenu}>Media</Link>
+          <Link to="/about" className='block py-2 px-2 text-sm hover:bg-red-400 hover:text-yellow-100' onClick={closeMobileMenu}>About</Link>
+          <Link to="/contact" className='block py-2 px-2 text-sm hover:bg-red-400 hover:text-yellow-100' onClick={closeMobileMenu}>Contact</Link>
+        </div>
       </nav>
     </div>
   );
