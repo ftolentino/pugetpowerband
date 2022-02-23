a71d2e9095b3a32138f2e2b0c6353d554859ee9b
ftolentino <filmer.tolentino@gmail.com>
2022-02-22 19:53:44 -0800

    set footer to scale in the window instead of the document. set PUGET POWER header responsive classes.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,9 +8,17 @@ export default function Layout({ children }) {
       <div>
         { children }
       </div>
-      <footer className="py-10 bg-aqua-teal flex flex-row justify-center">
+      <footer className="fixed bottom-0 w-full z-10 left-0 py-10 bg-aqua-teal flex flex-row justify-center">
         <p className="text-yellow-900">@Copyright 2022 Made with Gatsby</p>
       </footer>
     </div>
   );
 }
+
+/*
+position: fixed;
+bottom: 0;
+left: 0;
+width: 100%;
+z-index: 1;
+*/
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ import { StaticImage } from 'gatsby-plugin-image';
 export default function Home() {
   return (
     <Layout>
-      <section className="h-screen">
+      <section>
         <div className="flex flex-col items-center my-4">
-          <h1 className="sm:text-center font-bold tracking-wider text-9xl text-indigo-400">PUGET POWER</h1>
+          <h1 className="text-center text-5xl font-bold tracking-wider md:text-9xl text-indigo-400">PUGET POWER</h1>
         </div>
         <div>
           <StaticImage
